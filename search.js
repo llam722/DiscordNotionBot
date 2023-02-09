971e4796f2a12ae1acc71ccf32ff02b778d4424f
louislam <louislam7229@gmail.com>
2023-02-08 21:49:51 -0800

    first commit

---
+++ search.js
@@ -0,0 +1,18 @@
+import { Client } from '@notionhq/client'
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+(async () => {
+  const response = await notion.search({
+    query: 'External tasks',
+    filter: {
+      value: 'database',
+      property: 'object'
+    },
+    sort: {
+      direction: 'ascending',
+      timestamp: 'last_edited_time'
+    },
+  });
+  console.log(response);
+})();
