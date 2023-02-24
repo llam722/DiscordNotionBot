e6dadb7bc4a4e5eb5fb7c7a8220c560abf93d615
louislam <louislam7229@gmail.com>
2023-02-23 20:50:54 -0800

    convert UTC to local time for user

--- index.js
+++ index.js
@@ -21,13 +21,17 @@ async function addItem(text) {
         },
         Status: {
           status: {
-            name: "In progress",
+            name: "Done",
+          },
+        },
+        Date: {
+          date: {
+            start: toIsoString(new Date()),
           },
         },
       },
-    
     });
-    console.log(response);
+    console.log("response", response);
     console.log("Success! Entry added.");
   } catch (error) {
     console.error(error.body);
@@ -35,3 +39,30 @@ async function addItem(text) {
 }
 
 addItem("NY, New York");
+
+//helper function to convert ISOString into user's local timezone
+function toIsoString(date) {
+  let tzo = -date.getTimezoneOffset(),
+    dif = tzo >= 0 ? "+" : "-",
+    pad = function (num) {
+      return (num < 10 ? "0" : "") + num;
+    };
+
+  return (
+    date.getFullYear() +
+    "-" +
+    pad(date.getMonth() + 1) +
+    "-" +
+    pad(date.getDate()) +
+    "T" +
+    pad(date.getHours()) +
+    ":" +
+    pad(date.getMinutes()) +
+    ":" +
+    pad(date.getSeconds()) +
+    dif +
+    pad(Math.floor(Math.abs(tzo) / 60)) +
+    ":" +
+    pad(Math.abs(tzo) % 60)
+  );
+}
