7ff919895f9056568b6ebbe8d8834c0273894fc1
louislam <louislam7229@gmail.com>
2023-03-02 23:32:55 -0800

    add append to blockInput.js

--- events/blockInput.js
+++ events/blockInput.js
@@ -1,4 +1,8 @@
 const { Events } = require("discord.js");
+const { Client } = require("@notionhq/client");
+const { NOTION_KEY } = require("../config.json");
+const notion = new Client({ auth: NOTION_KEY });
+
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -15,6 +19,42 @@ module.exports = {
       const blockContent =
         interaction.fields.getTextInputValue("blockContentInput");
       console.log({ blockHeader, blockContent });
+      addItem(blockHeader, blockContent);
     }
+
+    async function addItem(blockHeader, blockContent) {
+      const blockId = "5f55ed6f-8af6-478e-9257-180878e8e2fe";
+      const response = await notion.blocks.children.append({
+        block_id: blockId,
+        children: [
+          {
+            heading_2: {
+              rich_text: [
+                {
+                  text: {
+                    content: blockHeader,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            paragraph: {
+              rich_text: [
+                {
+                  text: {
+                    content: blockContent,
+                    link: {
+                      url: "https://en.wikipedia.org/wiki/Lacinato_kale",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      });
+      console.log(response, "block content added to page!");
+    };
   },
 };
--- notion/pages/appendBlock.js
+++ notion/pages/appendBlock.js
@@ -3,6 +3,8 @@ const { NOTION_KEY } = require("../../config.json");
 
 const notion = new Client({ auth: NOTION_KEY });
 
+
+
 (async () => {
   const blockId = "4a5330c5-8cb7-4cf0-af55-49f1e6af2bf9";
   const response = await notion.blocks.children.append({
@@ -36,5 +38,5 @@ const notion = new Client({ auth: NOTION_KEY });
       },
     ],
   });
-  console.log(response);
+  console.log(response, 'block content added to page!');
 })();
