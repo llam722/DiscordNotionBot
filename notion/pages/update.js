873120c3d358a616af912dd52519af18dc3dadec
louislam <louislam7229@gmail.com>
2023-03-24 11:55:39 -0700

    update import in pages

--- notion/pages/update.js
+++ notion/pages/update.js
@@ -1,11 +1,10 @@
 const { Client } = require("@notionhq/client");
 const dotenv = require("dotenv");
 dotenv.config();
-
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
 const updateData = (async () => {
-  const pageId = "5f55ed6f-8af6-478e-9257-180878e8e2fe";
+  const pageId = "83af0783-30f8-4e89-a853-611cc2fe493e";
   const response = await notion.pages.update({
     page_id: pageId,
     properties: {
