1e16de7b7102f3be7659ab5f468fc08471591163
louislam <louislam7229@gmail.com>
2023-06-06 00:33:54 -0700

    add Notion client and imports to pageSelector

--- events/pageSelector.js
+++ events/pageSelector.js
@@ -1,5 +1,9 @@
 const { Events } = require("discord.js");
 const { pageId } = require("../tempPageId");
+const { Client } = require("@notionhq/client");
+const { NOTION_DATABASE_ID, NOTION_KEY } = require('../config.json')
+
+const notion = new Client({ auth: NOTION_KEY });
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -9,6 +13,27 @@ module.exports = {
     const selected = interaction.values[0];
     //logging interaction to see if I can obtain name of values without making another request
     // console.log('interaction', interaction)
+
+    const query = (async () => {
+      try {
+        const databaseId = NOTION_DATABASE_ID;
+        const response = await notion.databases.query({
+          database_id: databaseId,
+          property: "page",
+        });
+        const data = pageNameFinder(response);
+        console.log(data, 'data')
+        return data;
+      } catch (error) {
+        console.log(error, "page does not exist in database");
+      }
+    })();
+
+    const pageNameFinder = (pages) => {
+      pages.filter((page) => page.databaseId === selected)
+    }
+
+
 
     if (interaction.customId === "select") {
       await interaction.update({
